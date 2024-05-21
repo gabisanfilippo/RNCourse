fbf9b15a626066ed72d9e937bcde03893cf1fbc5
Gabrielle Coelho Sanfilippo <gabriellecoelhosanfilippo@gmail.com>
2024-05-21 13:19:45 -0300

    refactor: restyling

--- App.tsx
+++ App.tsx
@@ -1,50 +1,136 @@
 import { useState } from "react";
-import { Button, FlatList, StyleSheet, View } from "react-native";
+import { FlatList, Pressable, StyleSheet, Text, View } from "react-native";
 import { GoalItem } from "./components/GoalItem";
-import { GoalInput } from "./components/GoalInput";
+import { AddGoal } from "./components/AddGoal";
+
+export interface ITask {
+  text: string;
+  id: string;
+  time: string;
+  date: Date;
+  done: boolean;
+}
 
 export default function App() {
   const [modalIsVisible, setModalIsVisible] = useState(false);
-  const [courseGoals, setCourseGoals] = useState<
-    { text: string; id: string }[]
-  >([]);
+  const [courseGoals, setCourseGoals] = useState<ITask[]>([]);
+  const [hideCompleted, setHideCompleted] = useState(false);
 
-  function startAddGoalHandler() {
-    setModalIsVisible(true);
-  }
+  const todayList = courseGoals
+    .filter((task) => task.date.getDay() === new Date().getDay())
+    .filter((task) => (hideCompleted ? !task.done : task));
+  const tomorrowList = courseGoals.filter(
+    (task) => task.date.getDay() !== new Date().getDay()
+  );
 
   return (
     <View style={styles.appContainer}>
-      <Button
-        title="Add New Goal"
-        color={"#5e0acc"}
-        onPress={startAddGoalHandler}
-      />
-      <GoalInput
+      <View style={styles.titleContainer}>
+        <Text style={styles.title}>Today</Text>
+        <Pressable onPress={() => setHideCompleted((prev) => !prev)}>
+          <Text style={styles.hideText}>
+            {hideCompleted ? "Show" : "Hide"} completed
+          </Text>
+        </Pressable>
+      </View>
+      <View>
+        {todayList.length === 0 && (
+          <Text style={styles.nothing}>nothing to show.</Text>
+        )}
+        <FlatList
+          data={todayList}
+          keyExtractor={(item) => item.id}
+          renderItem={({ item }) => (
+            <GoalItem
+              mode="today"
+              item={item}
+              setCourseGoals={setCourseGoals}
+            />
+          )}
+        />
+      </View>
+      <Text style={styles.title}>Tomorrow</Text>
+
+      <View>
+        {tomorrowList.length === 0 && (
+          <Text style={styles.nothing}>nothing to show.</Text>
+        )}
+        <FlatList
+          data={tomorrowList}
+          keyExtractor={(item) => item.id}
+          renderItem={({ item }) => (
+            <GoalItem
+              mode="tomorrow"
+              item={item}
+              setCourseGoals={setCourseGoals}
+            />
+          )}
+        />
+      </View>
+
+      <Pressable
+        style={styles.addGoalContainer}
+        onPress={() => setModalIsVisible(true)}
+      >
+        <Text style={styles.addGoal}>+</Text>
+      </Pressable>
+      <AddGoal
         setCourseGoals={setCourseGoals}
         modalIsVisible={modalIsVisible}
         setModalIsVisible={setModalIsVisible}
       />
-      <View style={styles.goalsContainer}>
-        <FlatList
-          data={courseGoals}
-          keyExtractor={(item) => item.id}
-          renderItem={({ item }) => (
-            <GoalItem item={item} setCourseGoals={setCourseGoals} />
-          )}
-        />
-      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   appContainer: {
-    paddingTop: 50,
+    paddingVertical: 80,
     paddingHorizontal: 16,
     flex: 1,
+    gap: 16,
+    position: "relative",
+    backgroundColor: "#F7F8FA",
   },
   goalsContainer: {
     flex: 5,
+  },
+  title: {
+    fontSize: 32,
+    fontWeight: "900",
+  },
+  hideText: {
+    fontSize: 13,
+    fontWeight: "600",
+    color: "#3478F6",
+  },
+  titleContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "flex-end",
+  },
+  addGoal: {
+    color: "#FFFFFF",
+    fontSize: 30,
+    fontWeight: "600",
+    textAlign: "center",
+    lineHeight: 34,
+  },
+  addGoalContainer: {
+    backgroundColor: "#000000",
+    width: 40,
+    height: 40,
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 60,
+    position: "absolute",
+    right: 30,
+    bottom: 50,
+  },
+  nothing: {
+    fontSize: 16,
+    color: "#767577b9",
+    marginLeft: 40,
+    marginVertical: 30,
   },
 });
--- components/GoalItem.tsx
+++ components/GoalItem.tsx
@@ -1,40 +1,88 @@
 import { Dispatch, SetStateAction } from "react";
 import { Pressable, StyleSheet, Text, View } from "react-native";
+import { Checkbox } from "./Checkbox";
+import { ITask } from "../App";
 
 interface Props {
-  item: { text: string; id: string };
-  setCourseGoals: Dispatch<SetStateAction<{ text: string; id: string }[]>>;
+  mode: "today" | "tomorrow";
+  item: ITask;
+  setCourseGoals: Dispatch<SetStateAction<ITask[]>>;
 }
 
-export const GoalItem = ({ item, setCourseGoals }: Props) => {
-  function deleteGoalHandler(id: string) {
-    setCourseGoals((prev) => prev.filter((goal) => goal.id !== id));
+export const GoalItem = ({ item, setCourseGoals, mode }: Props) => {
+  function onChange() {
+    setCourseGoals((prev) =>
+      prev.map((task) =>
+        task.id !== item.id ? task : { ...task, done: !task.done }
+      )
+    );
   }
 
   return (
-    <View style={styles.goalItem}>
-      <Pressable
-        onPress={deleteGoalHandler.bind(this, item.id)}
-        android_ripple={{ color: "#230649" }}
-        style={({ pressed }) => pressed && styles.pressedItem}
-      >
-        <Text style={styles.goalText}>{item.text}</Text>
-      </Pressable>
+    <View style={styles.wrapper}>
+      {mode === "today" && (
+        <Checkbox isChecked={item.done} onChange={onChange} />
+      )}
+      {mode === "tomorrow" && (
+        <View style={styles.circleWrapper}>
+          <View style={styles.circle}></View>
+        </View>
+      )}
+
+      <View style={styles.taskWrapper}>
+        <Text
+          style={{
+            ...styles.text,
+            textDecorationLine: item.done ? "line-through" : "none",
+            opacity: item.done ? 0.3 : 1,
+          }}
+        >
+          {item.text}
+        </Text>
+        <Text
+          style={{
+            ...styles.time,
+            textDecorationLine: item.done ? "line-through" : "none",
+            opacity: item.done ? 0.3 : 1,
+          }}
+        >
+          {item.time}
+        </Text>
+      </View>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-  goalItem: {
-    margin: 8,
-    borderRadius: 6,
-    backgroundColor: "#5e0acc",
+  wrapper: {
+    flexDirection: "row",
+    alignItems: "center",
+    gap: 16,
+    marginBottom: 12,
+    maxWidth: "90%",
   },
-  goalText: {
-    padding: 8,
-    color: "#ffffff",
+  circleWrapper: {
+    width: 20,
+    justifyContent: "center",
+    alignItems: "center",
   },
-  pressedItem: {
-    opacity: 0.5,
+  circle: {
+    width: 10,
+    height: 10,
+    borderRadius: 46,
+    backgroundColor: "#000000",
+  },
+  taskWrapper: {
+    gap: 8,
+  },
+  text: {
+    color: "#737373",
+    fontSize: 15,
+    fontWeight: "400",
+  },
+  time: {
+    fontSize: 13,
+    fontWeight: "400",
+    color: "#A3A3A3",
   },
 });
